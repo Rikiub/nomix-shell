3f20191c9fa54c66dd1db7947f42fa53c8848ed3
Rikiub <rikiub.dev@gmail.com>
2025-02-17 18:49:56 -0400

    refactorize: bar clock

--- modules/bar/clock.py
+++ modules/bar/clock.py
@@ -9,6 +9,29 @@ from modules.user_options import user_options
 
 class Clock(Widget.Label):
     def __init__(self):
+        fmt = self.get_format()
+
+        super().__init__(
+            css_classes=["clock"],
+            label=Utils.Poll(
+                1000,
+                lambda _: datetime.datetime.now().strftime(fmt),
+            ).bind("output"),
+        )
+
+    def system_time_format(self) -> Literal["24", "12"]:
+        try:
+            locale.setlocale(locale.LC_TIME, "")
+            time_format = locale.nl_langinfo(locale.T_FMT)
+
+            if "%p" in time_format:
+                return "12"
+            else:
+                return "24"
+        except locale.Error:
+            return "24"
+
+    def get_format(self) -> str:
         options = user_options.clock
 
         # Time
@@ -29,24 +52,4 @@ class Clock(Widget.Label):
 
         date_format = f"{week}{'%B' if options.full_date else '%b'} %d"
 
-        super().__init__(
-            css_classes=["clock"],
-            label=Utils.Poll(
-                1000,
-                lambda _: datetime.datetime.now().strftime(
-                    f"{date_format}   {time_format}"
-                ),
-            ).bind("output"),
-        )
-
-    def system_time_format(self) -> Literal["24", "12"]:
-        try:
-            locale.setlocale(locale.LC_TIME, "")
-            time_format = locale.nl_langinfo(locale.T_FMT)
-
-            if "%p" in time_format:
-                return "12"
-            else:
-                return "24"
-        except locale.Error:
-            return "24"
+        return f"{date_format}   {time_format}"
