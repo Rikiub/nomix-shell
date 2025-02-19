750f61be005873f927b1fc6a2e7e57f1cdfb466d
Rikiub <rikiub.dev@gmail.com>
2025-02-19 19:16:42 -0400

    feat: dark mode listener on config.py

--- config.py
+++ config.py
@@ -7,12 +7,16 @@ from nomix.modules.launcher import Launcher
 from nomix.modules.notification_center import NotificationCenter
 from nomix.modules.notification_popup import NotificationPopup
 from nomix.modules.osd import OSD
+from nomix.services.color_scheme.service import ColorSchemeService
 from nomix.utils.constants import STYLES_DIR
 from nomix.utils.helpers import monitor_gtk4_css
 
 # SETUP
 app = IgnisApp.get_default()
 app.apply_css(str(STYLES_DIR / "index.scss"))
+
+# Dark mode switcher
+ColorSchemeService.get_default()
 
 # Auto reload in GTK4 theme changes
 monitor_gtk4_css()
