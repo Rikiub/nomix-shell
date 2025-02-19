21cfd4fec2a2da195afe3f6bf020c79eec1fa176
Rikiub <rikiub.dev@gmail.com>
2025-02-19 18:18:54 -0400

    feat!: improve ColorSchemeService
    - Now is forced use adw-gtk3 theme to avoid styles issues.
    - A fallback will be used if adw-gtk3 theme is not founded, but well, it's ugly.

--- nomix/services/color_scheme/constants.py
+++ nomix/services/color_scheme/constants.py
@@ -1,7 +1,20 @@
+import os
+from typing import Literal
 from nomix.utils.constants import STYLES_DIR
 
+COLOR_SCHEME = Literal["default", "prefer-light", "prefer-dark"]
+
 STYLE_FILE = STYLES_DIR / "lib" / "_dark.scss"
-STYLE_TEMPLATE = """
-/* DON'T MODIFY THIS FILE, WILL BE OVERWRITED */
-$is-dark: {};
-"""
+
+# Template for generated file
+STYLE_DISCLAIMER = "DON'T MODIFY THIS FILE, WILL BE OVERWRITED"
+STYLE_VARIABLE_NAME = "is-dark"
+
+# Only adw-gtk3 supported because styles bugs
+CHANGE_THEME = True
+GTK_THEME_LIGHT = "adw-gtk3"
+GTK_THEME_DARK = "adw-gtk3-dark"
+
+CHANGE_ICON = False
+GTK_ICON_LIGHT = os.getenv("GTK_ICON_LIGHT") or "Adwaita"
+GTK_ICON_DARK = os.getenv("GTK_ICON_DARK") or "Adwaita"
---
+++ nomix/services/color_scheme/service.py
@@ -0,0 +1,87 @@
+from gi.repository import Gio, GObject  # type: ignore
+from ignis.base_service import BaseService
+
+from .constants import (
+    CHANGE_ICON,
+    CHANGE_THEME,
+    COLOR_SCHEME,
+    GTK_ICON_DARK,
+    GTK_ICON_LIGHT,
+    GTK_THEME_DARK,
+    GTK_THEME_LIGHT,
+    STYLE_DISCLAIMER,
+    STYLE_FILE,
+    STYLE_VARIABLE_NAME,
+)
+
+
+class ColorSchemeService(BaseService):
+    def __init__(self) -> None:
+        super().__init__()
+        self._settings = Gio.Settings.new("org.gnome.desktop.interface")
+
+        self._color_scheme: COLOR_SCHEME = self._settings.get_string("color-scheme")  # type: ignore
+        self._is_dark: bool = False
+
+        self._sync()
+
+    @GObject.Property
+    def color_scheme(self) -> COLOR_SCHEME:  # type: ignore
+        return self._color_scheme
+
+    @color_scheme.setter
+    def color_scheme(self, value: COLOR_SCHEME) -> None:
+        self._color_scheme = value
+        self._sync()
+
+    @GObject.Property
+    def is_dark(self) -> bool:  # type: ignore
+        return self._is_dark
+
+    @is_dark.setter
+    def is_dark(self, value: bool) -> None:
+        if value:
+            self._color_scheme = "prefer-dark"
+        else:
+            self._color_scheme = "prefer-light"
+
+        self._sync()
+
+    def toggle(self) -> None:
+        if self._is_dark:
+            self._color_scheme = "prefer-light"
+        else:
+            self._color_scheme = "prefer-dark"
+
+        self._sync()
+
+    def _write_style(self) -> None:
+        STYLE_FILE.touch(exist_ok=True)
+
+        content = f"/* {STYLE_DISCLAIMER} */\n${STYLE_VARIABLE_NAME}: {'true' if self._is_dark else 'false'}"
+        STYLE_FILE.write_text(content)
+
+    def _sync(self):
+        if self._color_scheme == "prefer-dark":
+            if CHANGE_THEME:
+                self._settings.set_string("gtk-theme", GTK_THEME_DARK)
+            if CHANGE_ICON:
+                self._settings.set_string("icon-theme", GTK_ICON_DARK)
+            self._settings.set_string("color-scheme", "prefer-dark")
+
+            self._is_dark = True
+        elif self._color_scheme == "prefer-light":
+            if CHANGE_THEME:
+                self._settings.set_string("gtk-theme", GTK_THEME_LIGHT)
+            if CHANGE_ICON:
+                self._settings.set_string("icon-theme", GTK_ICON_LIGHT)
+            self._settings.set_string("color-scheme", "prefer-light")
+
+            self._is_dark = False
+        else:
+            return
+
+        self.notify("color_scheme")
+        self.notify("is_dark")
+
+        self._write_style()
