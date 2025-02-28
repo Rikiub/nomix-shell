8f2bad22d07f1edb5e306e26506f87d9cf8ec0dd
Rikiub <rikiub.dev@gmail.com>
2025-02-28 01:57:39 -0400

    :sparkles: (bar): Improve hover effect feedback on buttons

--- nomix/widgets/actionable_button.py
+++ nomix/widgets/actionable_button.py
@@ -13,30 +13,50 @@ class ActionableButton(Widget.EventBox):
     def __init__(
         self,
         on_click: Callable | None = None,
+        on_hover: Callable | None = None,
+        on_hover_lost: Callable | None = None,
         toggle_window: ModuleWindow | None = None,
         css_classes: list[str] = [],
         child: Binding | BaseWidget | None = None,
         **kwargs,
     ):
         self.__on_click = on_click
+        self.__on_hover = on_hover
+        self.__on_hover_lost = on_hover_lost
         self.__toggle_window = toggle_window
 
         self._button = Widget.Button(css_classes=css_classes, child=child)
 
         super().__init__(
-            on_click=lambda v: self._callback(v), child=[self._button], **kwargs
+            on_click=lambda v: self._proxy_on_click(v),
+            on_hover=lambda v: self._proxy_on_hover(v),
+            on_hover_lost=lambda v: self._proxy_on_hover_lost(v),
+            child=[self._button],
+            **kwargs,
         )
 
         if toggle_window:
             self._window = app.get_window(toggle_window)
             self._window.connect("notify::visible", lambda *_: self._toggle_active())
 
-    def _callback(self, value):
+    def _proxy_on_click(self, value):
         if self.__on_click:
             self.__on_click(value)
 
         if self.__toggle_window:
             app.toggle_window(self.__toggle_window)
+
+    def _proxy_on_hover(self, value):
+        self._button.add_css_class("hover")
+
+        if self.__on_hover:
+            self.__on_hover(value)
+
+    def _proxy_on_hover_lost(self, value):
+        self._button.remove_css_class("hover")
+
+        if self.__on_hover_lost:
+            self.__on_hover_lost(value)
 
     def _toggle_active(self):
         if self._window.is_visible():
