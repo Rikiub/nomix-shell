0f76d16f3afbfa7007de93166b4efd16858b72e7
Rikiub <rikiub.dev@gmail.com>
2025-03-06 15:43:12 -0400

    :bug: Move to async functions

--- nomix/modules/notification_center/media.py
+++ nomix/modules/notification_center/media.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from ignis.services.mpris import MprisPlayer, MprisService
 from ignis.utils import Utils
 from ignis.widgets import Widget
@@ -19,9 +21,12 @@ class Player(Widget.Revealer):
             app_name = desktop.name
             app_icon = desktop.symbolic_icon
 
+        def play_pause():
+            asyncio.create_task(self._player.play_pause_async())
+
         header = Widget.EventBox(
             css_classes=["notification-header"],
-            on_click=lambda _: self._player.play_pause(),
+            on_click=lambda _: play_pause(),
             child=[
                 Widget.Icon(image=app_icon, css_classes=["app-icon"]),
                 Widget.Label(label=app_name, css_classes=["app-name"]),
@@ -30,7 +35,7 @@ class Player(Widget.Revealer):
 
         picture_size = 50
         picture = Widget.EventBox(
-            on_click=lambda _: self._player.play_pause(),
+            on_click=lambda _: play_pause(),
             child=[
                 Widget.Picture(
                     image=self._player.bind("art_url"),
@@ -46,7 +51,7 @@ class Player(Widget.Revealer):
         metadata = Widget.EventBox(
             vertical=True,
             hexpand=True,
-            on_click=lambda _: self._player.play_pause(),
+            on_click=lambda _: play_pause(),
             css_classes=["metadata"],
             child=[
                 Widget.Label(
@@ -78,12 +83,14 @@ class Player(Widget.Revealer):
             child=[
                 Widget.Button(
                     css_classes=["action-button"],
-                    on_click=lambda _: self._player.previous(),
+                    on_click=lambda _: asyncio.create_task(
+                        self._player.previous_async()
+                    ),
                     child=Widget.Icon(image="media-skip-backward-symbolic"),
                 ),
                 Widget.Button(
                     css_classes=["action-button"],
-                    on_click=lambda _: self._player.play_pause(),
+                    on_click=lambda _: play_pause(),
                     visible=self._player.bind("can_play"),
                     child=Widget.Icon(
                         image=self._player.bind(
@@ -96,7 +103,7 @@ class Player(Widget.Revealer):
                 ),
                 Widget.Button(
                     css_classes=["action-button"],
-                    on_click=lambda _: self._player.next(),
+                    on_click=lambda _: asyncio.create_task(self._player.next_async()),
                     visible=self._player.bind("can_go_next"),
                     child=Widget.Icon(image="media-skip-forward-symbolic"),
                 ),
@@ -110,14 +117,16 @@ class Player(Widget.Revealer):
 
         progress = Widget.EventBox(
             css_classes=["progress-bar"],
-            on_click=lambda _: self._player.play_pause(),
+            on_click=lambda _: play_pause(),
             visible=player.bind("position", lambda value: value != -1),
             child=[
                 Widget.Label(label=self._player.bind("position", lambda v: minutes(v))),
                 Widget.Scale(
                     max=self._player.bind("length"),
                     value=self._player.bind("position"),
-                    on_change=lambda x: self._player.set_position(x.value),
+                    on_change=lambda x: asyncio.create_task(
+                        self._player.set_position_async(x.value)
+                    ),
                     hexpand=True,
                 ),
                 Widget.Label(label=self._player.bind("length", lambda v: minutes(v))),
@@ -140,7 +149,7 @@ class Player(Widget.Revealer):
 
     def _destroy(self):
         self.reveal_child = False
-        Utils.Timeout(self.transition_duration, super().unparent)
+        Utils.Timeout(self.transition_duration, self.unparent)
 
 
 class MediaPlayer(Widget.Box):
